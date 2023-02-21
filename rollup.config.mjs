bbe60dfa66850d3c4b36d5b1e7e2e6f398a260a2
jest-games-llc <jest.games.llc@gmail.com>
2023-02-21 10:45:23 -0500

    [frontend-package-object-helpers #19]
    Update rollup config to use external
    
    Closes #19

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json' assert { type: "json" };
-import babel from '@rollup/plugin-babel';
-import dts from 'rollup-plugin-dts';
+// import babel from '@rollup/plugin-babel';
+// import dts from 'rollup-plugin-dts';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import external from 'rollup-plugin-peer-deps-external';
+// import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.js', '.ts'];
 
@@ -10,22 +13,25 @@ export default [
   {
     input: './src/index.ts',
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true },
     ],
     plugins: [
+      external(),
       resolve({ extensions }),
-      babel({ exclude: 'node_modules/**', extensions, babelHelpers: 'bundled' }),
       commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      // babel({ exclude: 'node_modules/**', extensions, babelHelpers: 'bundled' }),
+      // terser(),
     ],
     onwarn: (warning) => {
       if (warning.code === 'THIS_IS_UNDEFINED') return;
       console.warn(warning.message);
     },
   },
-  {
-    input: './dist/types/index.d.ts',
-    output: [{ file: pkg.types, format: 'es' }],
-    plugins: [dts()],
-  },
+  // {
+  //   input: './dist/types/index.d.ts',
+  //   output: [{ file: pkg.types, format: 'es' }],
+  //   plugins: [dts()],
+  // },
 ];
